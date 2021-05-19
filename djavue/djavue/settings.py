219c7301677eb3d90edae8cb1acdd7e9ee80000b
HassanBahati <mukisabahati@gmail.com>
2021-05-19 10:01:50 +0300

    added media paths to settings/

--- djavue/djavue/settings.py
+++ djavue/djavue/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 #configuring the cors headers
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080'
+    'http://localhost:8000'
 ]
 
 MIDDLEWARE = [
@@ -131,8 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
