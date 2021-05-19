355b9cb8a10f244bd42ea1ad11a880668eb6844e
HassanBahati <mukisabahati@gmail.com>
2021-05-19 10:05:19 +0300

    added sedia conf to urls/

--- djavue/djavue/urls.py
+++ djavue/djavue/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -7,5 +8,6 @@ urlpatterns = [
     #for authentications
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
-]
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+ 
