11499728e36befd3b3e2f6ed767858f3d79408ba
HassanBahati <mukisabahati@gmail.com>
2021-05-22 21:01:21 +0300

    added serializer

--- djavue/product/serializers.py
+++ djavue/product/serializers.py
@@ -1,1 +1,21 @@
 from rest_framework import serializers
+ 
+'''
+ serializers get data from db and turn it into json so it can be used in frontend
+'''
+
+from .models import Category, Product
+
+class productSerializer(serializers.ModelSerializer):
+    #to configure the product serializer 
+    class Meta:
+        model = Product
+        fields = {
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail"
+        }
